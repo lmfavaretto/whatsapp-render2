249c470ad957f663fa20ac0e0ae06eb4d86cd0dd
lmfavaretto <55554697+lmfavaretto@users.noreply.github.com>
2025-03-30 18:19:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     puppeteer: {
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
-client.on('qr', qr => {
-    console.log('QR RECEIVED', qr);
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
